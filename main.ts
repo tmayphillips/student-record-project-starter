0c5eee10d45346aaf885f03b270611b787ddcfed
Terra Taylor <terra.taylor@gmail.com>
2021-11-24 11:19:04 -0600

    init commit

---
+++ main.ts
@@ -0,0 +1,32 @@
+/* Create a function to add student data to an array as an array of 
+objects and render objects to the page
+
+Be sure your function parameters are given strict types
+
+*/
+
+/* Define your data structure using a custom Type.
+https://www.digitalocean.com/community/tutorials/how-to-create-custom-types-in-typescript
+
+Student
+    First name (string)
+    Last name (string)
+    Course  (string)
+    Grade (number or string)
+    isPassing (boolean value if grade is greater than a D)
+
+    If student is passing, render a green symbol/icon next to their entry in the table
+    If student is not passing, render a red symbol/icon next to their entry in the table
+
+    It is up to you to calculate based on grade (numerical or letter) if student is passing or not
+
+
+    Data should be rendered in the form of a table, i.e.,
+
+    |First Name|Last Name|Course|Grade (as Letter)|Passing?|
+    | Leon     |Kennedy  |RE-101|  B              |   :)   |
+
+
+    Add a button that sorts the data based on Grade (ascending order)
+    Add a button that sorts teh data based on Course (ascending order)
+*/
